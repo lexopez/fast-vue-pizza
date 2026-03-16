8f4ae40bfefa81215cae3b335701fd4a24edb024
alexander lopez <iamlexlopez@gmail.com>
2026-03-16 23:42:30 +0800

    submitting menu in the cart page and clearing cart functionality

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -5,17 +5,22 @@ import SharedButton from '@/components/SharedButton.vue'
 import UpdateItemQuantity from '@/components/UpdateItemQuantity.vue'
 import DeleteItem from '@/components/DeleteItem.vue'
 import { formatCurrency } from '@/utils/helpers'
+import { useUserStore } from '@/stores/userStore'
+import { useCartStore } from '@/stores/cartStore'
+import EmptyCart from '@/components/EmptyCart.vue'
+const userStore = useUserStore()
+const cartStore = useCartStore()
 </script>
 
 <template>
-  <div class="px-4 py-3">
+  <div v-if="cartStore.cart.length" class="px-4 py-3">
     <RouterLink to="/menu">&larr; Back to menu</RouterLink>
 
-    <h2 class="mt-7 text-xl font-semibold">Your cart, {username}</h2>
+    <h2 class="mt-7 text-xl font-semibold">Your cart, {{ userStore.username }}</h2>
 
     <ul class="mt-3 divide-y divide-stone-200 border-b">
       <li
-        v-for="(item, index) in cart"
+        v-for="(item, index) in cartStore.cart"
         :key="index"
         className="py-3 sm:flex sm:items-center sm:justify-between"
       >
@@ -23,16 +28,19 @@ import { formatCurrency } from '@/utils/helpers'
         <div className="flex items-center justify-between sm:gap-6">
           <p className="text-sm font-bold">{{ formatCurrency(item.totalPrice) }}</p>
 
-          <UpdateItemQuantity />
-          <DeleteItem />
+          <UpdateItemQuantity :pizzaId="item.pizzaId" />
+          <DeleteItem :pizzaId="item.pizzaId" />
         </div>
       </li>
     </ul>
 
     <div class="mt-6 space-x-2">
-      <SharedButton to="/order/new" type="primary"> Order pizzas </SharedButton>
+      <RouterLink to="/order/new">
+        <SharedButton type="primary"> Order pizzas </SharedButton>
+      </RouterLink>
 
-      <SharedButton type="secondary"> Clear cart </SharedButton>
+      <SharedButton type="secondary" @trigger="cartStore.clearCart"> Clear cart </SharedButton>
     </div>
   </div>
+  <EmptyCart v-else />
 </template>
